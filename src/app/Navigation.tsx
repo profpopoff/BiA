b245a21fe9319e8214d840dd9fc7f4d6a8a79376
Aleksei Ivanov <profpopoff@gmail.com>
2022-11-03 20:12:10 +0300

    Add link hover animation

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -18,27 +18,36 @@ export default function Navigation({ navActive, setNavActive }: { navActive: boo
             <div className="nav-links">
                <Link className="link" href="/" onClick={toggleNavigation}>
                   <h2 className="link-label">home</h2>
-                  <Image className="link-image" alt="home" src={Cover} />
+                  <div className="link-image">
+                     <Image className="src" alt="home" src={Cover} />
+                  </div>
                </Link>
                <Link className="link" href="/cases" onClick={toggleNavigation}>
-               <h2 className="link-label">work</h2>
-                  <Image className="link-image" alt="home" src={Cover} />
+                  <h2 className="link-label">work</h2>
+                  <div className="link-image">
+                     <Image className="src" alt="home" src={Cover} />
+                  </div>
                </Link>
-               {/* <Link href={`/case/${id}`}>case</Link> */}
                <Link className="link" href="/about" onClick={toggleNavigation}>
-               <h2 className="link-label">about</h2>
-                  <Image className="link-image" alt="home" src={Cover} />
+                  <h2 className="link-label">about</h2>
+                  <div className="link-image">
+                     <Image className="src" alt="home" src={Cover} />
+                  </div>
                </Link>
                <Link className="link" href="/contact" onClick={toggleNavigation}>
-               <h2 className="link-label">contact</h2>
-                  <Image className="link-image" alt="home" src={Cover} />
+                  <h2 className="link-label">contact</h2>
+                  <div className="link-image">
+                     <Image className="src" alt="home" src={Cover} />
+                  </div>
                </Link>
                <Link className="link" href="/" onClick={toggleNavigation}>
-               <h2 className="link-label">join us</h2>
-                  <Image className="link-image" alt="home" src={Cover} />
+                  <h2 className="link-label">join us</h2>
+                  <div className="link-image">
+                     <Image className="src" alt="home" src={Cover} />
+                  </div>
                </Link>
             </div>
-         </nav>
+         </nav >
 
       </>
    )
