6e6691a012c73207179f5c542111e91195113df3
Aleksei Ivanov <profpopoff@gmail.com>
2022-11-07 23:04:10 +0300

    Create cases style file

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image"
+import styles from './Cases.module.css'
 
 export default function Cases() {
    return (
       <div>
-         <h1>
+         <h1 className={styles.title}>
             cases page
          </h1>
       </div>
