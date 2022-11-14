cf3e27aa3079b13ec9968a39fbb3bde9fd1e8956
Aleksei Ivanov <profpopoff@gmail.com>
2022-11-14 15:26:41 +0300

    Add array divide function

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -1,12 +1,31 @@
 import Image from "next/image"
-import styles from './Cases.module.css'
+import styles from './Cases.module.scss'
 
 export default function Cases() {
+
+   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+   const divideArray = (array: any[]) => {
+      const array1 = array.slice(0, Math.floor(array.length / 2)),
+         array2 = array.slice(Math.floor(array.length / 2))
+      return [array1, array2]
+   }
+
+   // divideArray(array).map((half) => {
+   //    divideArray(half).map(quarter => {
+   //       console.log(quarter, quarter.length)
+   //    })
+   // })
+
    return (
-      <div>
-         <h1 className={styles.title}>
-            cases page
-         </h1>
+      <div className={styles.container}>
+         {divideArray(array).map((half) =>
+            divideArray(half).map(quarter =>
+               <div className={styles.gallery}>
+                  {quarter.map((item: number) => <span>{item}</span>)}
+               </div>
+            )
+         )}
       </div>
    )
 }
