64506729622b496eedbd55eb568ee7b5e740032a
Aleksei Ivanov <profpopoff@gmail.com>
2022-11-17 14:14:26 +0300

    Adjust divideArray function

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -5,13 +5,10 @@ import Filter from "./Filter"
 
 export default function Cases() {
 
-   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-   const divideArray = (array: any[]) => {
-      const array1 = array.slice(0, Math.floor(array.length / 2)),
-         array2 = array.slice(Math.floor(array.length / 2))
-      return [array1, array2]
-   }
+   const divideArray = (array: any[]) =>
+      ([array.slice(0, Math.floor(array.length / 2)), array.slice(Math.floor(array.length / 2))])
 
    return (
       <div className={styles.container}>
