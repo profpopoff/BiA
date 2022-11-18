91f635bb2ca640a2553dc290ad2fd7f649a70321
Aleksei Ivanov <profpopoff@gmail.com>
2022-11-18 11:53:08 +0300

    Move divideArray to utils, add quarterArray func quarterArray func makes first array elements apear on top and in the center, moving to sides and then bottom

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -1,26 +1,21 @@
 import Image from "next/image"
-import styles from './Cases.module.scss'
 
+import styles from './Cases.module.scss'
+import { quarterArray } from "../../utils/divideArray"
 import Filter from "./Filter"
 
 export default function Cases() {
 
-   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
-
-   const divideArray = (array: any[]) =>
-      ([array.slice(0, Math.floor(array.length / 2)), array.slice(Math.floor(array.length / 2))])
+   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,]
 
    return (
       <div className={styles.container}>
          <Filter />
          <div className={styles.galleries}>
-            {divideArray(array).map((half) =>
-               divideArray(half).map(quarter =>
-                  <div className={styles.gallery}>
-                     {quarter.map((item: number) => <span>{item}</span>)}
-                  </div>
-               )
-            )}
+            {quarterArray(array).map(quarter =>
+               <div className={styles.gallery}>
+                  {quarter.map((item: number) => <span>{item}</span>)}
+               </div>)}
          </div>
       </div>
    )
---
+++ src/utils/divideArray.ts
@@ -0,0 +1,29 @@
+export const divideArray = (array: any[]) => ([array.slice(0, Math.floor(array.length / 2)), array.slice(Math.floor(array.length / 2))])
+
+export const quarterArray = (array: any[]) => {
+
+   let counter = 0
+   let array1 = [], array2 = [], array3 = [], array4 = []
+
+   for (let i of array) {
+
+      switch (counter) {
+         case 0:
+            array2.push(i)
+            break
+         case 1:
+            array3.push(i)
+            break
+         case 2:
+            array1.push(i)
+            break
+         case 3:
+            array4.push(i)
+            break
+      }
+
+      counter === 3 ? counter = 0 : ++counter
+   }
+
+   return [array1, array2, array3, array4]
+}
