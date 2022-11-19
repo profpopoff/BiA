6c8b094ee3731a3e274c90977c1b9dacc01c832c
Aleksei Ivanov <profpopoff@gmail.com>
2022-11-19 10:34:00 +0300

    Make artist name conditional

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -10,7 +10,16 @@ import Cover from '../../public/cover.jpeg'
 export default function Cases() {
 
    // const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,]
-   const array = [{ title: 'Между светом и тенью', artist: 'Евгений Гороховский', dates: ['1.1.2021', '1.1.2022'], image: '' }, { title: 'Где-то рай', artist: 'Виктор Норкин', dates: ['1.1.2021', '1.1.2022'], image: '' }, { title: 'Выход за пределы', artist: 'MARCK', dates: ['1.1.2021', '1.1.2022'], image: '' }, { title: 'Блестящая эпоха', artist: 'Альберт Тибурци', dates: ['1.1.2021', '1.1.2022'], image: '' }, { title: 'Маленькая девочка и кот Том', artist: 'Андрей Прохоров', dates: ['1.1.2021', '1.1.2022'], image: '' },]
+   const array = [
+      { title: 'Между светом и тенью', artist: 'Евгений Гороховский', dates: ['1.1.2021', '1.1.2022'] },
+      { title: 'Где-то рай', artist: 'Виктор Норкин', dates: ['1.1.2021', '1.1.2022'] },
+      { title: 'Выход за пределы', artist: 'MARCK', dates: ['1.1.2021', '1.1.2022'] },
+      { title: 'Блестящая эпоха', artist: 'Альберт Тибурци', dates: ['1.1.2021', '1.1.2022'] },
+      { title: 'Маленькая девочка и кот Том', artist: 'Андрей Прохоров', dates: ['1.1.2021', '1.1.2022'] },
+      { title: 'Последний титан', artist: 'Андрей Карташов', dates: ['1.1.2021', '1.1.2022'] },
+      { title: 'Арт-синергия', dates: ['1.1.2021', '1.1.2022'] },
+      { title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
+   ]
 
    return (
       <div className={styles.container}>
@@ -19,11 +28,11 @@ export default function Cases() {
             {quarterArray(array).map(quarter =>
                <div className={styles.gallery}>
                   {quarter.map((item) =>
-                     <Link href='/' className={styles.item}>
+                     <Link href='/' className={styles.card}>
                         {/* <Image className={styles.image} src={Cover} alt="cover" /> */}
                         <div className={styles.headline}>
                            <h2 className={styles.title}>{item.title}</h2>
-                           <span className={styles.artist}>{item.artist}</span>
+                           {!!item.artist && <span className={styles.artist}>{item.artist}</span>}
                         </div>
                      </Link>
                   )}
