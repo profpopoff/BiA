95318017ae044a9052ab25b5df0242f81c1a2bed
Aleksei Ivanov <profpopoff@gmail.com>
2022-11-20 23:17:19 +0300

    Add paralax effect to cases gallery

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Image from "next/image"
 import Link from "next/link"
+import React, { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react"
 
 import styles from './Cases.module.scss'
 import { quarterArray } from "../../utils/divideArray"
@@ -9,7 +12,6 @@ import Cover from '../../public/cover.jpeg'
 
 export default function Cases() {
 
-   // const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,]
    const array = [
       { title: 'Между светом и тенью', artist: 'Евгений Гороховский', dates: ['1.1.2021', '1.1.2022'] },
       { title: 'Где-то рай', artist: 'Виктор Норкин', dates: ['1.1.2021', '1.1.2022'] },
@@ -29,25 +31,52 @@ export default function Cases() {
       { title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
       { title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
       { title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-   ]
+   ] // perfect length = 18
+
+   const itemsRef = useRef<Array<HTMLDivElement | null>>([])
+
+   const [heights, setHeights] = useState<any>()
+
+   useEffect(() => {
+      itemsRef.current = itemsRef.current.slice(0, quarterArray(array).length)
+
+      setHeights([...itemsRef.current.map((item: any) => item.clientHeight)])
+   }, [])
+
+   const scrollHandler = (e: any) => {
+      for (let i: number = 0; i <= heights.length; i++) {
+         if (itemsRef.current[i]) {
+            const scrollY = e.currentTarget.scrollTop
+            const yDecimal = scrollY / (e.target.scrollHeight - e.target.offsetHeight)
+            const maxY = heights[i] - e.target.scrollHeight
+            const panY = maxY * yDecimal
+            itemsRef.current[i]!.style.transform = `translateY(${-panY}px)`
+         }
+      }
+   }
 
    return (
-      <div className={styles.container}>
-         <Filter />
-         <div className={styles.galleries}>
-            {quarterArray(array).map(quarter =>
-               <div className={styles.gallery}>
-                  {quarter.map((item) =>
-                     <Link href='/' className={styles.card}>
-                        {/* <Image className={styles.image} src={Cover} alt="cover" /> */}
-                        <div className={styles.headline}>
-                           <h2 className={styles.title}>{item.title}</h2>
-                           {!!item.artist && <span className={styles.artist}>{item.artist}</span>}
-                        </div>
-                     </Link>
-                  )}
-               </div>)}
+      <div className="container"
+         onScroll={scrollHandler}>
+         <div className={styles.wrapper}>
+            <Filter />
+            <div className={styles.galleries}>
+               {quarterArray(array).map((quarter, index) =>
+                  <div className={styles.gallery} key={index}
+                     ref={el => itemsRef.current[index] = el}>
+                     {quarter.map((item, index) =>
+                        <Link href='/' className={styles.card} key={index}>
+                           {/* <Image className={styles.image} src={Cover} alt="cover" /> */}
+                           <div className={styles.headline}>
+                              <h2 className={styles.title}>{item.title}</h2>
+                              {!!item.artist && <span className={styles.artist}>{item.artist}</span>}
+                           </div>
+                        </Link>
+                     )}
+                  </div>
+               )}
+            </div>
          </div>
-      </div>
+      </div >
    )
 }
