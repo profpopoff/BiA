a4773645adde472656c03c00ddef1c735c5fa750
Aleksei Ivanov <profpopoff@gmail.com>
2022-12-09 15:49:22 +0300

    Add arithmetic sequence to decide array length so that paralax effect is stable and somewhat nice no matter how big the array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,28 +18,35 @@ export default async function HomePage() {
       { id: 5, title: 'Маленькая девочка и кот Том', artist: 'Андрей Прохоров', dates: ['1.1.2021', '1.1.2022'] },
       { id: 6, title: 'Последний титан', artist: 'Андрей Карташов', dates: ['1.1.2021', '1.1.2022'] },
       { id: 7, title: 'Арт-синергия', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 8, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 9, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 10, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 11, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 12, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 13, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 14, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 15, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 16, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 17, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 18, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 19, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
-      { id: 20, title: 'Pan-opticum', artist: 'Ван О', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 8, title: 'Pan-opticum', artist: 'Ван 8', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 9, title: 'Pan-opticum', artist: 'Ван 9', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 10, title: 'Pan-opticum', artist: 'Ван 10', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 11, title: 'Pan-opticum', artist: 'Ван 11', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 12, title: 'Pan-opticum', artist: 'Ван 12', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 13, title: 'Pan-opticum', artist: 'Ван 13', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 14, title: 'Pan-opticum', artist: 'Ван 14', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 15, title: 'Pan-opticum', artist: 'Ван 15', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 16, title: 'Pan-opticum', artist: 'Ван 16', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 17, title: 'Pan-opticum', artist: 'Ван 17', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 18, title: 'Pan-opticum', artist: 'Ван 18', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 19, title: 'Pan-opticum', artist: 'Ван 19', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 20, title: 'Pan-opticum', artist: 'Ван 20', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 21, title: 'Pan-opticum', artist: 'Ван 21', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 22, title: 'Pan-opticum', artist: 'Ван 22', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 23, title: 'Pan-opticum', artist: 'Ван 23', dates: ['1.1.2021', '1.1.2022'] },
+      { id: 24, title: 'Pan-opticum', artist: 'Ван 24', dates: ['1.1.2021', '1.1.2022'] },
+      // { id: 25, title: 'Pan-opticum', artist: 'Ван 25', dates: ['1.1.2021', '1.1.2022'] },
+      // { id: 26, title: 'Pan-opticum', artist: 'Ван 26', dates: ['1.1.2021', '1.1.2022'] },
    ] // perfect length = 18
 
    return (
       <div className="container">
          <div className={styles.wrapper}>
             <div className={styles.galleries}>
-               {countOff(array.slice(0, 17), 2).map((gallery: any[]) =>
-                  <Gallery gallery={gallery} />
-               )}
+               {countOff(array.slice(0, 2 * Math.floor((array.length - 3) / 2) + 3), 2) // Арифметическая последовательность 1,3,5,7,9... (чтобы паралакс был красивым (стабильным))
+                  .map((events: any[]) =>
+                     <Gallery events={events} />
+                  )}
             </div>
             <Article />
          </div>
