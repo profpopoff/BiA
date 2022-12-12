74b5973e1cad9e4ef4bf70c83b1d141286f46217
Aleksei Ivanov <profpopoff@gmail.com>
2022-12-12 16:09:57 +0300

    Add 'priority' to image

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -81,25 +81,25 @@ export default function Navigation() {
             <Link className={navigation.link} href="/" onClick={toggleNav}>
                <h2 className={pathname == "/" ? `${navigation.linkLabel} ${navigation.linkActive}` : navigation.linkLabel}>home</h2>
                <div className={navigation.linkImage}>
-                  <Image className={navigation.src} alt="home" src={Cover} />
+                  <Image className={navigation.src} alt="home" src={Cover} priority />
                </div>
             </Link>
             <Link className={navigation.link} href="/cases" onClick={toggleNav}>
                <h2 className={pathname == "/cases" ? `${navigation.linkLabel} ${navigation.linkActive}` : navigation.linkLabel}>work</h2>
                <div className={navigation.linkImage}>
-                  <Image className={navigation.src} alt="home" src={Cover} />
+                  <Image className={navigation.src} alt="home" src={Cover} priority />
                </div>
             </Link>
             <Link className={navigation.link} href="/about" onClick={toggleNav}>
                <h2 className={pathname == "/about" ? `${navigation.linkLabel} ${navigation.linkActive}` : navigation.linkLabel}>about</h2>
                <div className={navigation.linkImage}>
-                  <Image className={navigation.src} alt="home" src={Cover} />
+                  <Image className={navigation.src} alt="home" src={Cover} priority />
                </div>
             </Link>
             <Link className={navigation.link} href="/contact" onClick={toggleNav}>
                <h2 className={pathname == "/contact" ? `${navigation.linkLabel} ${navigation.linkActive}` : navigation.linkLabel}>contact</h2>
                <div className={navigation.linkImage}>
-                  <Image className={navigation.src} alt="home" src={Cover} />
+                  <Image className={navigation.src} alt="home" src={Cover} priority />
                </div>
             </Link>
             <Link className={navigation.link} href="/" onClick={toggleNav}>
