106c7672aca65439064cdc5aac3fff8f074e8f60
Aleksei Ivanov <profpopoff@gmail.com>
2022-12-13 20:50:42 +0300

    Add pathName check to determine handler action

--- src/app/components/MainButton/MainButton.tsx
+++ src/app/components/MainButton/MainButton.tsx
@@ -7,16 +7,21 @@ import { faBarsStaggered, faArrowRotateBack } from '@fortawesome/free-solid-svg-
 import { InteractionContext } from "../../context/InteractionContext"
 
 import mainButton from './MainButton.module.scss'
+import { usePathname, useRouter } from "next/navigation"
 
 export default function MainButton() {
 
-   const { nav, toggleNav, filterActive, toggleFilter, caseActive, toggleCase } = useContext(InteractionContext)
+   const { nav, toggleNav, filterActive, toggleFilter } = useContext(InteractionContext)
+
+   const pathname = usePathname()
+
+   const router = useRouter()
 
    const clickHandler = () => {
-      if (filterActive) {
+      if (pathname?.startsWith("/event/")) {
+         router.push('/events')
+      } else if (filterActive) {
          toggleFilter?.()
-      } else if (caseActive) {
-         toggleCase?.()
       } else {
          toggleNav?.()
       }
@@ -24,10 +29,10 @@ export default function MainButton() {
 
    return (
       <button
-         className={(nav || filterActive || caseActive) ? `${mainButton.button} ${mainButton.active}` : mainButton.button}
+         className={(nav || filterActive || pathname?.startsWith("/event/")) ? `${mainButton.button} ${mainButton.active}` : mainButton.button}
          onClick={clickHandler}
       >
-         {(nav || filterActive || caseActive) ?
+         {(nav || filterActive || pathname?.startsWith("/event/")) ?
             <FontAwesomeIcon icon={faArrowRotateBack} className={mainButton.icon} /> :
             <FontAwesomeIcon icon={faBarsStaggered} className={mainButton.icon} />}
       </button>
