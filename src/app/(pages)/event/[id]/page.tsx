6523083e8744e4328ba2123e2a36f417260aee7a
Aleksei Ivanov <profpopoff@gmail.com>
2022-12-16 17:00:10 +0300

    Add info section

--- src/app/(pages)/event/[id]/page.tsx
+++ src/app/(pages)/event/[id]/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image"
+import { decode } from 'html-entities'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faCakeCandles, faCalendarDays } from '@fortawesome/free-solid-svg-icons'
+
 import { fetchEvent } from "../../../../utils/fetch"
 
 import eventStyle from './Event.module.scss'
@@ -9,9 +13,20 @@ export default async function Event({ params }: {
 
    const { data } = await fetchEvent(params.id)
 
+   const zoneToRus = (zone: string) => {
+      switch (zone) {
+         case 'exposition':
+            return 'Выставочное крыло'
+         case 'museum':
+            return 'Музейнок крыло'
+         case 'stage':
+            return 'Сцена'
+      }
+   }
+
    return (
       <div className={eventStyle.container}>
-         <div className={eventStyle.hero}>
+         <section className={eventStyle.hero}>
             <Image
                className={eventStyle.image}
                src={data.images[0]}
@@ -23,7 +38,44 @@ export default async function Event({ params }: {
                <span className={eventStyle.title}>{data.title}</span>
                {!!data.artist && <span className={eventStyle.artist}>{data.artist}</span>}
             </h2>
-         </div>
+         </section>
+         <section className={eventStyle.info}>
+            <div className={eventStyle.infoWrapper}>
+               <article>
+                  <h2 className={eventStyle.type}>Временная выставка</h2>
+                  <p className={eventStyle.description}>
+                     <span className={eventStyle.textWrapper}>
+                        {decode(data.description)}
+                     </span>
+                  </p>
+               </article>
+               <div className={eventStyle.stats}>
+                  <div className={eventStyle.stat}>
+                     <FontAwesomeIcon icon={faCalendarDays} className={eventStyle.icon} />
+                     {`${new Date(data.dates.start).toLocaleDateString()} - ${new Date(data.dates.end).toLocaleDateString()}`}
+                  </div>
+                  <ul className={eventStyle.statsList}>
+                     <li className={eventStyle.stat}>
+                        <FontAwesomeIcon icon={faLocationDot} className={eventStyle.icon} />
+                        {`${zoneToRus(data.place.zone)}, ${data.place.floor} этаж`}
+                     </li>
+                     <li className={eventStyle.stat}>
+                        <FontAwesomeIcon icon={faCakeCandles} className={eventStyle.icon} />
+                        {data.ageRestriction}+
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <div className={eventStyle.src}>
+               <Image
+                  className={eventStyle.image}
+                  src={data.images[1]}
+                  fill={true}
+                  sizes='auto'
+                  alt={`${data.title} image`}
+               />
+            </div>
+         </section>
       </div>
    )
 }
