4200563a7dc36e716e8764b5d273c541e6323d52
Aleksei Ivanov <profpopoff@gmail.com>
2022-12-21 21:47:05 +0300

    Add function that fetchs next event

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -15,3 +15,30 @@ export async function fetchEvent(id: string) {
 
    return eventResponse.json()
 }
+
+export async function fetchNextEvent(id: string) {
+   const events = await fetch("http://localhost:3000/api/events", { cache: "no-store" })
+      .then(eventsResponse => eventsResponse.json())
+
+   let nextEvent: { title: string, image: string, id: string } = { title: '', image: '', id: '' }
+
+   events.data.map((event: any, index: number) => {
+      if (event._id === id) {
+         if (index + 1 < events.data.length) {
+            nextEvent = {
+               id: events.data[index + 1]._id,
+               title: events.data[index + 1].title,
+               image: events.data[index + 1].images.cover,
+            }
+         } else {
+            nextEvent = {
+               id: events.data[0]._id,
+               title: events.data[0].title,
+               image: events.data[0].images.cover,
+            }
+         }
+      }
+   })
+
+   return nextEvent
+}
