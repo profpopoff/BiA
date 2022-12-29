0273d0ae0258966cd16bf03509a726851c11cf1c
Aleksei Ivanov <profpopoff@gmail.com>
2022-12-29 17:05:05 +0300

    Update auto scroll Now it works onScroll with timer (somewhy it stoped working onWheel)

--- src/app/(pages)/(event)/event/[slug]/components/Wrapper.tsx
+++ src/app/(pages)/(event)/event/[slug]/components/Wrapper.tsx
@@ -1,81 +1,52 @@
 'use client'
 
-import React, { useEffect, useRef, useState } from "react"
+import React, { useRef, useState } from "react"
 
 export default function Wrapper({ children }: { children: React.ReactNode }) {
 
    const containerRef = useRef<HTMLDivElement>(null)
 
-   const [scrollTrigger, setScrollTrigger] = useState(false)
+   var timer: any = null
+   const [scrollY, setScrollY] = useState(0)
 
-   const [nextSection, setNextSection] = useState(0)
+   const onScroll = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
 
-   const [touchStart, setTouchStart] = useState(null)
-   const [touchEnd, setTouchEnd] = useState(null)
-
-   const minSwipeDistance = 50
-
-   useEffect(() => {
-      containerRef.current?.children[nextSection].scrollIntoView({ behavior: 'smooth' })
-   }, [scrollTrigger])
-
-   const onWheel = (e: any) => {
-
-      const { scrollTop, clientHeight } = containerRef.current!
-
-      if (e.nativeEvent.deltaY > 0) {
-         if (scrollTop > clientHeight * nextSection - clientHeight * .5 &&
-            nextSection < e.currentTarget.children.length - 1) {
-            setNextSection(currentSection => currentSection + 1)
-            setScrollTrigger(prevScroll => !prevScroll)
-         } else {
-            setScrollTrigger(prevScroll => !prevScroll)
-         }
-      } else {
-         if (scrollTop < clientHeight * nextSection + clientHeight * .5 &&
-            nextSection > 0) {
-            setNextSection(currentSection => currentSection - 1)
-            setScrollTrigger(prevScroll => !prevScroll)
-         } else {
-            setScrollTrigger(prevScroll => !prevScroll)
-         }
+      if (timer !== null) {
+         clearTimeout(timer)
       }
-   }
 
-   const onTouchStart = (e: any) => {
-      setTouchEnd(null)
-      setTouchStart(e.targetTouches[0].clientY)
-   }
+      timer = setTimeout(function () {
 
-   const onTouchMove = (e: any) => setTouchEnd(e.targetTouches[0].clientY)
+         const container = e.target as HTMLInputElement
+         const { scrollTop, clientHeight } = container
+         const scrollHeight = scrollY - scrollTop
+         const scrollTo = (scrollTop - clientHeight * .25) / clientHeight
 
-   const onTouchEnd = (e: any) => {
-
-      if (!touchStart || !touchEnd) return
-      const distance = touchStart - touchEnd
-
-      if (distance > minSwipeDistance) {
-         if (nextSection < e.currentTarget.children.length - 1) {
-            setNextSection(currentSection => currentSection + 1)
-            setScrollTrigger(prevScroll => !prevScroll)
+         if (scrollHeight >= clientHeight * .25) {
+            container.scrollTo({
+               top: Math.round(scrollTo) * clientHeight,
+               behavior: 'smooth'
+            })
+         } else if (scrollHeight <= -clientHeight * .25) {
+            container.scrollTo({
+               top: Math.ceil(scrollTo) * clientHeight,
+               behavior: 'smooth'
+            })
+         } else {
+            container.scrollTo({
+               top: (Math.floor(scrollTo) + 1) * clientHeight,
+               behavior: 'smooth'
+            })
          }
-      } else if (distance < -minSwipeDistance) {
-         if (nextSection > 0) {
-            setNextSection(currentSection => currentSection - 1)
-            setScrollTrigger(prevScroll => !prevScroll)
-         }
-      } else {
-         setScrollTrigger(prevScroll => !prevScroll)
-      }
+
+         setScrollY(scrollTop)
+      }, 150)
    }
 
    return (
       <div className="container"
          ref={containerRef}
-         onWheel={onWheel}
-         onTouchStart={onTouchStart}
-         onTouchMove={onTouchMove}
-         onTouchEnd={onTouchEnd}
+         onScroll={onScroll}
       >
          {children}
       </div >
