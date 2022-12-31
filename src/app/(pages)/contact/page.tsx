983df8a80e1a488d2b3c2b38e815c0f7d96db7e7
Aleksei Ivanov <profpopoff@gmail.com>
2022-12-31 17:55:10 +0300

    Add images to contact page

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,5 +1,30 @@
-export default function Contact() {
+import { countOff } from '../../../utils/divideArray'
+import { fetchEvents } from '../../../utils/fetch'
+import Gallery from '../../components/Gallery/Gallery'
+import contactStyle from './Contact.module.scss'
+
+
+export default async function Contact() {
+
+   const { data } = await fetchEvents()
+
    return (
-      <div>contact page</div>
+      <div className="container">
+         <div className={contactStyle.wrapper}>
+            <div className={contactStyle.galleries}>
+               {countOff(
+                  data.map((el: any) => el.images.cover) // images toot
+                  , 2).map((images: string[], index: number) =>
+                     <Gallery key={index} array={images} />)}
+            </div>
+            <Contacts />
+         </div>
+      </div>
+   )
+}
+
+const Contacts = () => {
+   return (
+      <div className={contactStyle.info}>contacts</div>
    )
 }
