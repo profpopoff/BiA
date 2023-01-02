cd2b90d0088b1c2f2409362c60c269c55ef945e3
Aleksei Ivanov <profpopoff@gmail.com>
2023-01-02 22:16:42 +0300

    Add contact info

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -23,8 +23,48 @@ export default async function Contact() {
    )
 }
 
-const Contacts = () => {
-   return (
-      <div className={contactStyle.info}>contacts</div>
-   )
-}
+const Contacts = () => (
+   <div className={contactStyle.contacts}>
+      <h1>Cвяжитесь с&nbsp;нами</h1>
+      <div className={contactStyle.links}>
+         <div>
+            <h2>Посетите</h2>
+            <a href="https://yandex.ru/maps/-/CCUrfEWi3B" target="_blank" className={contactStyle.link}>
+               <span className={contactStyle.dot}></span>
+               <span className={contactStyle.text}>Васильевский остров, 29-я линия, дом 2&nbsp;| 199106 Санкт-Петербург</span>
+            </a>
+         </div>
+         <div>
+            <h2>Напишите</h2>
+            <a href="mailto:info@erarta.com" target="_blank" className={contactStyle.link}>
+               <span className={contactStyle.dot}></span>
+               <span className={contactStyle.text}>info@erarta.com</span>
+            </a>
+         </div>
+         <div>
+            <h2>Позвоните</h2>
+            <a href="tel:+7 (812) 324 08 09" target="_blank" className={contactStyle.link}>
+               <span className={contactStyle.dot}></span>
+               <span className={contactStyle.text}>+7 (812) 324 08&nbsp;09</span>
+            </a>
+         </div>
+         <div>
+            <h2>Следите за новостями</h2>
+            <div className={contactStyle.socials}>
+               <a href="https://vk.com/erarta" target="_blank" className={contactStyle.link}>
+                  <span className={contactStyle.dot}></span>
+                  <span className={contactStyle.text}>Вконтакте</span>
+               </a>
+               <a href="https://t.me/erarta_museum" target="_blank" className={contactStyle.link}>
+                  <span className={contactStyle.dot}></span>
+                  <span className={contactStyle.text}>Телеграм</span>
+               </a>
+               <a href="https://www.youtube.com/user/Erartamuseum" target="_blank" className={contactStyle.link}>
+                  <span className={contactStyle.dot}></span>
+                  <span className={contactStyle.text}>YouTube</span>
+               </a>
+            </div>
+         </div>
+      </div>
+   </div>
+)
