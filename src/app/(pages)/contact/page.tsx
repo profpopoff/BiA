22692dfd0ba924edd4f2b17223ddfcd2d2ae9710
Aleksei Ivanov <profpopoff@gmail.com>
2023-01-03 13:15:41 +0300

    Add museum related images

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,21 +1,17 @@
 import { countOff } from '../../../utils/divideArray'
-import { fetchEvents } from '../../../utils/fetch'
 import Gallery from '../../components/Gallery/Gallery'
 import contactStyle from './Contact.module.scss'
+import images from '../../../public/img/contacts/contactsImages'
 
 
-export default async function Contact() {
-
-   const { data } = await fetchEvents()
+export default function Contact() {
 
    return (
       <div className="container">
          <div className={contactStyle.wrapper}>
             <div className={contactStyle.galleries}>
-               {countOff(
-                  data.map((el: any) => el.images.cover) // images toot
-                  , 2).map((images: string[], index: number) =>
-                     <Gallery key={index} array={images} />)}
+               {countOff(images, 2).map((images: string[], index: number) =>
+                  <Gallery key={index} array={images} />)}
             </div>
             <Contacts />
          </div>
---
+++ src/public/img/contacts/contactsImages.js
@@ -0,0 +1,8 @@
+import img1 from './img1.jpeg'
+import img2 from './img2.jpeg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpeg'
+
+export default [img1, img2, img3, img4, img5, img6]
