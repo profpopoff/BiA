dd7d3509a1287c82898ff317a70d41f0adfb56ee
Aleksei Ivanov <profpopoff@gmail.com>
2023-01-05 22:29:17 +0300

    Start about page

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,5 +1,32 @@
+import Image from 'next/image'
+
+import Wrapper from '../(event)/event/[slug]/components/Wrapper'
+import aboutStyles from './About.module.scss'
+
+import img from '../../../public/img/about/img2.jpeg'
+
 export default function About() {
    return (
-      <div>about page</div>
+      <div className={aboutStyles.about}>
+         <Wrapper>
+            <section className={aboutStyles.hero}>
+               <Image
+                  className={aboutStyles.image}
+                  src={img}
+                  fill={true}
+                  sizes='100vw'
+                  alt={`image`}
+               />
+               {/* <h2 className={aboutStyles.headline}>
+               </h2> */}
+            </section>
+            <section>
+               <h1>section 2</h1>
+            </section>
+            <section>
+               <h1>section 3</h1>
+            </section>
+         </Wrapper>
+      </div>
    )
 }
