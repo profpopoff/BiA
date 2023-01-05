b6eb6a8202281f5babe619103454f6521ce8b3eb
Aleksei Ivanov <profpopoff@gmail.com>
2023-01-05 22:23:48 +0300

    Add showOff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function HomePage() {
          <div className={styles.wrapper}>
             <div className={styles.galleries}>
                {countOff(data
-                  .slice(0, 2 * Math.floor((data.length - 3) / 2) + 3), 2) 
+                  .slice(0, 2 * Math.floor((data.length - 3) / 2) + 3), 2)
                   .map((events: any[], index: number) => <Gallery key={index} array={events} />)}
             </div>
             <Article />
@@ -24,6 +24,7 @@ export default async function HomePage() {
 
 const Article = () => (
    <article className={styles.article}>
+      <span className={styles.showOff}>Первый и&nbsp;крупнейший музей современного искусства <span>Санкт-Петербурга</span></span>
       <h1 className={styles.slogan}>
          <span className={styles.marginLeft}>мир велик</span>
          <span>и никто</span>
