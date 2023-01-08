cc2fff09f3aabaed0f008b20de5186e9698cb04e
Aleksei Ivanov <profpopoff@gmail.com>
2023-01-08 12:57:56 +0300

    Add Info section

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -3,23 +3,17 @@ import Image from 'next/image'
 import Wrapper from '../(event)/event/[slug]/components/Wrapper'
 import aboutStyles from './About.module.scss'
 
-import img from '../../../public/img/about/img2.jpeg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faClock, faCalendarDays } from '@fortawesome/free-solid-svg-icons'
+
+import img from '../../../public/img/about/img7.jpg'
+import CustomLink from '../../components/CustomLink/CustomLink'
 
 export default function About() {
    return (
       <div className={aboutStyles.about}>
          <Wrapper>
-            <section className={aboutStyles.hero}>
-               <Image
-                  className={aboutStyles.image}
-                  src={img}
-                  fill={true}
-                  sizes='100vw'
-                  alt={`image`}
-               />
-               {/* <h2 className={aboutStyles.headline}>
-               </h2> */}
-            </section>
+            <Info />
             <section>
                <h1>section 2</h1>
             </section>
@@ -30,3 +24,40 @@ export default function About() {
       </div>
    )
 }
+
+const Info = () => (
+   <section className={aboutStyles.hero}>
+      <div className={aboutStyles.info}>
+         <article>
+            <h1>Первый и&nbsp;крупнейший музей современного искусства в&nbsp;Санкт-Петербурге</h1>
+            <p>Музей открылся 30&nbsp;сентября 2010&nbsp;года, и&nbsp;с&nbsp;этого момента в&nbsp;его стенах началась интересная и&nbsp;насыщенная жизнь&nbsp;&mdash; выставки, экскурсии, театральные постановки, концерты, образовательные мероприятия и&nbsp;многое другое.</p>
+            <CustomLink href='/contact'>
+               <span className={aboutStyles.text}>Контакты</span>
+            </CustomLink>
+         </article>
+         <ul>
+            <li>
+               <FontAwesomeIcon icon={faClock} className={aboutStyles.icon} />
+               С&nbsp;10:00 до&nbsp;22:00
+            </li>
+            <li>
+               <FontAwesomeIcon icon={faCalendarDays} className={aboutStyles.icon} />
+               Ежедневно, кроме вторника 
+            </li>
+            <li>
+               <FontAwesomeIcon icon={faLocationDot} className={aboutStyles.icon} />
+               Васильевский остров, 29-я линия, дом 2&nbsp;| 199106 Санкт-Петербург
+            </li>
+         </ul>
+      </div>
+      <div className={aboutStyles.image}>
+         <Image
+            className={aboutStyles.src}
+            src={img}
+            fill={true}
+            sizes='100vw'
+            alt='image'
+         />
+      </div>
+   </section>
+)
