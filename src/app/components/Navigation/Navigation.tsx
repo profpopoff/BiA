7f4ea5fd80453936b401515d1aec46b40c810d8f
Aleksei Ivanov <profpopoff@gmail.com>
2023-01-23 17:49:13 +0300

    Add mobile version images

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -1,10 +1,8 @@
 'use client'
 
-import Image from "next/image"
 import { usePathname, useRouter } from "next/navigation"
 import { useContext, useEffect, useRef, useState } from "react"
 import { InteractionContext } from "../../context/InteractionContext"
-import images from '../../../public/img/navigation/navigationImages'
 
 import navigation from './Navigation.module.scss'
 
@@ -69,6 +67,23 @@ export default function Navigation() {
       setTimeout(() => router.push(link), 500)
    }
 
+   const [windowWidth, setWindowWidth] = useState<Number>()
+
+   const handleWindowWidthChange = (event: UIEvent) => {
+      const w = event.target as Window
+      setWindowWidth(w.innerWidth)
+   }
+
+   useEffect(() => {
+      setWindowWidth(window.innerWidth)
+
+      window.addEventListener('resize', handleWindowWidthChange)
+
+      return () => {
+         window.addEventListener('resize', handleWindowWidthChange)
+      }
+   }, [])
+
    return (
       <nav className={navigation.container}>
          <div
@@ -86,27 +101,19 @@ export default function Navigation() {
          >
             <div className={navigation.link} onClick={() => handleClick('/')}>
                <h2 className={pathname == "/" ? `${navigation.linkLabel} ${navigation.linkActive}` : navigation.linkLabel}>Главная</h2>
-               <div className={navigation.linkImage}>
-                  <Image className={navigation.src} alt="home" src={images.home} priority />
-               </div>
+               <div className={`${navigation.home} ${navigation.linkImage}`}></div>
             </div>
             <div className={navigation.link} onClick={() => handleClick('/events')}>
                <h2 className={pathname == "/events" ? `${navigation.linkLabel} ${navigation.linkActive}` : navigation.linkLabel}>Афиша</h2>
-               <div className={navigation.linkImage}>
-                  <Image className={navigation.src} alt="home" src={images.events} priority />
-               </div>
+               <div className={`${navigation.linkImage} ${navigation.events}`}></div>
             </div>
             <div className={navigation.link} onClick={() => handleClick('/about')}>
                <h2 className={pathname == "/about" ? `${navigation.linkLabel} ${navigation.linkActive}` : navigation.linkLabel}>О музее</h2>
-               <div className={navigation.linkImage}>
-                  <Image className={navigation.src} alt="home" src={images.about} priority />
-               </div>
+               <div className={`${navigation.linkImage} ${navigation.about}`}></div>
             </div>
             <div className={navigation.link} onClick={() => handleClick('/contact')}>
                <h2 className={pathname == "/contact" ? `${navigation.linkLabel} ${navigation.linkActive}` : navigation.linkLabel}>Контакты</h2>
-               <div className={navigation.linkImage}>
-                  <Image className={navigation.src} alt="home" src={images.contact} priority />
-               </div>
+               <div className={`${navigation.linkImage} ${navigation.contact}`}></div>
             </div>
          </div>
       </nav >
--- src/public/img/navigation/navigationImages.js
+++
@@ -1,6 +0,0 @@
-import home from './home.png'
-import events from './events.png'
-import contact from './contact.png'
-import about from './about.png'
-
-export default { home, events, contact, about }
