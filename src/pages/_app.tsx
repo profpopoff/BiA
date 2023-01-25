bd679fa185b56319312a969b304393763a8dc6b6
Aleksei Ivanov <profpopoff@gmail.com>
2023-01-25 10:16:49 +0300

    Add loading indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from 'next/app'
 import '../style/index.scss'
 import InteractionProvider from '../context/InteractionContext'
 
+import NextNProgress from 'nextjs-progressbar'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
    return (
       <InteractionProvider>
+         <NextNProgress color="#D00439" height={0} />
          <Component {...pageProps} />
       </InteractionProvider>
    )
